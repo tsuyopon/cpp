4a7279f318d03b1cdaf05c87e3c01cdb28a2d61d
Tsuyoshi Ooishi <tsuyoppy@r2.dion.ne.jp>
2012-12-08 19:42:21 +0900

    added

---
+++ libxml2/sample1/xpath_sample1.cc
@@ -0,0 +1,59 @@
+/*
+ * Parse RSS Sample using XPath.
+ *
+ * (Reference)
+ *     http://d.hatena.ne.jp/hakutoitoi/20090319/1237397160
+ *
+ */
+#include <stdio.h>
+#include <libxml/xmlreader.h>
+#include <libxml/xpath.h>
+
+enum Status {
+    Success = 0,
+    Fail    = -1,
+};
+
+const char* REQUEST_API    = "http://messages.yahoo.co.jp/bbs?action=4&board=552019556&sid=552019556&tid=kcc08bc0obafb3u";
+const char* XPATH_ITEM     = "/rss//item/*/text()";
+const char* XPATH_TITLE    = "/rss//item/title/text()";
+const char* XPATH_PUB_DATE = "/rss//item/pubDate/text()";
+    
+int main(void) {
+
+	// Read XML From REQUEST_API
+    xmlTextReaderPtr reader =  xmlNewTextReaderFilename(REQUEST_API);
+    if (!reader) return Fail;
+
+    xmlTextReaderRead(reader);
+    xmlTextReaderExpand(reader);
+    xmlDocPtr doc = xmlTextReaderCurrentDoc(reader);
+    if (!doc) return Fail;
+
+    xmlXPathContextPtr ctx = xmlXPathNewContext(doc);
+    if (!ctx) return Fail;
+    
+    xmlXPathObjectPtr xpobj = xmlXPathEvalExpression((xmlChar *)XPATH_ITEM, ctx);
+    if (!xpobj) return Fail;
+
+    xmlNodeSetPtr nodes = xpobj->nodesetval;
+    int size = (nodes) ? nodes->nodeNr : 0;
+    for (int i = 0; i < size; ++i) {
+        if (!xmlXPathNodeSetIsEmpty(nodes)) {
+            xmlNodePtr node = xmlXPathNodeSetItem(nodes, i);
+            if (node->content) {
+                printf("%s => %s\n", node->parent->name, node->content);
+            } else {
+                printf("invalid node\n");
+                return Fail;
+            }
+        }
+    }
+
+    xmlXPathFreeObject(xpobj);
+    xmlXPathFreeContext(ctx);
+    xmlFreeDoc(doc);
+    xmlFreeTextReader(reader);
+    xmlCleanupParser();
+    return Success;
+}
---
+++ libxml2/sample1/xpath_sample2.cc
@@ -0,0 +1,51 @@
+/*
+ * Parse RSS using xpath.
+ * Improvement version of the xpath_sample1.cc. Check diff between xpath_sample1.cc.
+ *
+ */
+#include <stdio.h>
+#include <libxml/xmlreader.h>
+#include <libxml/xpath.h>
+
+enum Status {
+    Success = 0,
+    Fail    = -1,
+};
+
+const char* REQUEST_API    = "http://messages.yahoo.co.jp/bbs?action=4&board=552019556&sid=552019556&tid=kcc08bc0obafb3u";
+const char* XPATH_ITEM     = "/rss//item/*/text()";
+const char* XPATH_TITLE    = "/rss//item/title/text()";
+const char* XPATH_PUB_DATE = "/rss//item/pubDate/text()";
+    
+int main(void) {
+
+	// Read XML From REQUEST_API
+	xmlDocPtr doc = xmlParseFile(REQUEST_API);
+    if (!doc) return Fail;
+
+    xmlXPathContextPtr ctx = xmlXPathNewContext(doc);
+    if (!ctx) return Fail;
+    
+    xmlXPathObjectPtr xpobj = xmlXPathEvalExpression((xmlChar *)XPATH_ITEM, ctx);
+    if (!xpobj) return Fail;
+
+    xmlNodeSetPtr nodes = xpobj->nodesetval;
+    int size = (nodes) ? nodes->nodeNr : 0;
+    for (int i = 0; i < size; ++i) {
+        if (!xmlXPathNodeSetIsEmpty(nodes)) {
+            xmlNodePtr node = xmlXPathNodeSetItem(nodes, i);
+            if (node->content) {
+                printf("%s => %s\n", node->parent->name, node->content);
+            } else {
+                printf("invalid node\n");
+                return Fail;
+            }
+        }
+    }
+
+    xmlXPathFreeObject(xpobj);
+    xmlXPathFreeContext(ctx);
+    xmlFreeDoc(doc);
+    xmlCleanupParser();
+    return Success;
+}
