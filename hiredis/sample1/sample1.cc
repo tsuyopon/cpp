83e9cca45ec0f939ce4a0c44cc7d090cab598cf8
Tsuyoshi Ooishi <tsuyoppy@r2.dion.ne.jp>
2013-06-19 02:51:16 +0900

    modified

--- hiredis/sample1/sample1.cc
+++ hiredis/sample1/sample1.cc
@@ -29,14 +29,29 @@ int main(void) {
 
         redisReply *reply_set;
         reply_set = (redisReply*)redisCommand(c,"set host %s", id);
+        if( reply_set == NULL ){
+            // This error handling is needed so as not to cause segmentation fault.
+            printf("If Redis server could not reply, redisCommand() will return NULL\n");
+            redisFree(c);
+            exit(0);
+        }
         freeReplyObject(reply_set);
 
         redisReply *reply_host;
         reply_host = (redisReply*)redisCommand(c,"get host");
+        if( reply_set == NULL ){
+            printf("If Redis server could not reply, redisCommand() will return NULL\n");
+            redisFree(c);
+            exit(0);
+        }
+
         if (reply_host->type == REDIS_REPLY_STRING) {
             printf("hostid of %s is %s\n", hostname,reply_host->str);
         }
         freeReplyObject(reply_host);
+
+        // If forgot redisFree(), redis server's file-descripter will be exhausted.
+        redisFree(c);
 
         return 1;
 }
