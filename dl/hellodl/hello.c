7fac8f8b79aa50541259dc183f4eee71d2be2202
tsuyopon <tsuyoppy@r2.dion.ne.jp>
2015-02-07 22:53:55 +0900

    added dl sample

---
+++ dl/hellodl/hello.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int
+hello (int i)
+{
+	printf ("Hello, %d\n", i);
+	return (i + 1);
+}
---
+++ dl/hellodl/main.c
@@ -0,0 +1,37 @@
+// http://kaworu.jpn.org/kaworu/2008-11-26-1.php
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <dlfcn.h>
+
+int
+main(int argc, char *argv[], char *environ[])
+{
+	void	*h;
+	int	(*f) (int);
+	
+	// dlopenで共有ライブラリhello.soをロードします。
+	h = dlopen ("./hello.so", RTLD_LAZY);
+	if (h == NULL)
+	{
+		fprintf (stderr, "%s", dlerror() );
+		exit (EXIT_FAILURE);
+	}
+
+    // dlsymで共有ライブラリのhello関数ポインタを取得します
+	f = dlsym (h, "hello");
+
+	if (f == NULL)
+	{
+		fprintf (stderr, "%s\n", dlerror() );
+		exit (EXIT_FAILURE);
+	}
+
+    // dlsymで取得した関数ポインタを指定して実行する
+	printf ("%d\n", (f) (200));
+
+	// 不要になったライブラリを閉じる
+	dlclose (h);
+
+	exit (EXIT_SUCCESS);
+}
