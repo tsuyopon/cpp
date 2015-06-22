e3abd6749ae5d8c194a7b95f50435f137b0643d1
tsuyopon <tsuyoppy@r2.dion.ne.jp>
2015-06-23 03:22:00 +0900

    modified

--- desginpattern/singleton/sample1/Singleton.h
+++ desginpattern/singleton/sample1/Singleton.h
@@ -3,9 +3,12 @@ using namespace std;
 
 class SingletonClass {
 	private:
+		// ポイント1: コンストラクタをprivateにする。コピーコンストラクタをprivateにする。
 		SingletonClass(void) {}
-		static SingletonClass* singleton;           // 変数がstaticで宣言されているのがポイント。なんど呼びだしても同じ位置をさしている
+		// ポイント2: privateでstaticな自分自身のポインタ型をメンバ変数に追加する。
+		static SingletonClass* singleton;
 	public:
+		// ポイント3: publicでstaticな自分自身へのポインタを返す関数をメンバ関数に追加する。
 		static SingletonClass* getInstance(void) {  // 
 			return singleton;
 		}
--- desginpattern/singleton/sample1/main.cpp
+++ desginpattern/singleton/sample1/main.cpp
@@ -3,9 +3,14 @@
 #include "Singleton.h"
 
 int main(void) {
+
+	// getInstance()を通じてSingletonClassにアクセスできるようになります。
     SingletonClass* sc = SingletonClass::getInstance();
 	SingletonClass::getInstance()->doSomething(100);
+
+	// アドレスからの呼び出しはドット(.)ではなくアロー演算子(->)を用います
     sc->start();
+    sc->doSomething(1);
     sc->doSomething(1);
     sc->end();
 }
