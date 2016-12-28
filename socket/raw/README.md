cb3771d7df9158dade3fdfff08b88f776d3efb29
Tsuyoshi Ooishi <tsuyoppy@r2.dion.ne.jp>
2016-12-28 09:54:35 +0900

    add

---
+++ socket/raw/README.md
@@ -0,0 +1,8 @@
+# 概要
+RAWソケットを使ったサンプル
+
+# 参考
+- Manage of PACKET
+ - https://linuxjm.osdn.jp/html/LDP\_man-pages/man7/packet.7.html
+- RAWソケットを使った受信 
+ - http://linux-biyori.sakura.ne.jp/program/pr\_rawsock.php
---
+++ socket/raw/rawdump1.c
@@ -0,0 +1,79 @@
+/*
+ * 概要:
+ * RAWソケットでのhelloworld的なプログラムです。
+ * さらにパケット情報も見たければこれを改良したrawdump2.cを参考にしてください。
+ *
+ * [使い方]
+ *   $ ./a.out    // この後にエンターが押下されて初めてキャプチャーが開始されます。
+ *
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <arpa/inet.h>
+#include <net/ethernet.h>
+#include <errno.h>
+
+int main(void) {
+	struct ether_header *eth;
+	struct iphdr *ip;
+	char buf[ETHER_MAX_LEN] = {0};
+	char smac[20], dmac[20];
+	char sip[16], dip[16];
+	int sockfd = 0, len = 0, protocol = 0;
+
+	// SOCK_RAWを指定することによりRAWを受信することができる。
+	// htons(ETH_P_ALL)が指定されると全てのプロトコルが指定されたのと同様　
+	sockfd = socket(PF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
+	if(sockfd < 0){
+		perror("socket error");
+		return -1;
+	}
+
+	while(1){
+		len = recv(sockfd, buf, sizeof(buf), 0);
+		if(len < 0){
+			perror("recv error");
+			break;
+		}
+		eth = (struct ether_header *)buf;
+		sprintf(dmac, "%02x:%02x:%02x,%02x:%02x:%02x",
+			eth->ether_dhost[0], eth->ether_dhost[1],
+			eth->ether_dhost[2], eth->ether_dhost[3],
+			eth->ether_dhost[4], eth->ether_dhost[5]);
+		sprintf(smac, "%02x:%02x:%02x,%02x:%02x:%02x",
+			eth->ether_shost[0], eth->ether_shost[1],
+			eth->ether_shost[2], eth->ether_shost[3],
+			eth->ether_shost[4], eth->ether_shost[5]);
+		protocol = ntohs(eth->ether_type);
+		fprintf(stdout, "\nMAC : %s >> %s (Protocol = %04x, size = %dbyte)\n",
+			smac, dmac, protocol, len);
+		switch(protocol){
+		case ETHERTYPE_IP:
+			ip = (struct iphdr *)(buf + sizeof(struct ether_header));
+			sprintf(sip, "%s", inet_ntoa(*(struct in_addr *)&(ip->saddr)));
+			sprintf(dip, "%s", inet_ntoa(*(struct in_addr *)&(ip->daddr)));
+			fprintf(stdout, "IP %s > %s (%dbyte)\n",
+				sip, dip, ntohs(ip->tot_len));
+			break;
+		case ETHERTYPE_ARP:
+			fprintf(stdout, "ARP\n");
+			break;
+		case ETHERTYPE_REVARP:
+			fprintf(stdout, "Reverse ARP\n");
+			break;
+		default:
+			fprintf(stdout, "other protocol\n");
+			break;
+		}
+
+		memset(buf, 0, len);
+	}
+	close(sockfd);
+	return 0;
+}
