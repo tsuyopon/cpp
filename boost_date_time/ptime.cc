8cd5cb0803e36013b5f6b24e8b2c92eca6df97c1
tsuyopon <tsuyoppy@r2.dion.ne.jp>
2017-04-26 08:43:11 +0900

    added

--- boost_date_time/ptime.cc
+++ boost_date_time/ptime.cc
@@ -1,3 +1,4 @@
+// 現在時刻を様々な形式で取得する
 #include <iostream>
 #include <boost/date_time/posix_time/posix_time.hpp>
 using namespace std;
@@ -6,8 +7,9 @@ int main()
 {
 	typedef boost::posix_time::ptime PTime;
 	typedef boost::posix_time::second_clock Clock;
-	PTime time = Clock::local_time();
+	PTime time = Clock::local_time();   // その地域を取得する
 
+	// 現在の時刻・日付・時・分・秒を取得する
 	cout << time << endl;
 	cout << time.date() << endl;
 	cout << time.time_of_day() << endl;
@@ -15,6 +17,7 @@ int main()
 	cout << time.time_of_day().minutes() << endl;
 	cout << time.time_of_day().seconds() << endl;
 
+	// 4時間加算した時刻の日付を表示する
 	time = time + boost::posix_time::hours(4);
 	cout << time.time_of_day() << endl;
 
