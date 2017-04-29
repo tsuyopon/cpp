fc3222b4341ca9cb3e2a3ee36728b65390b35ad8
tsuyopon <tsuyoppy@r2.dion.ne.jp>
2017-04-30 02:34:51 +0900

    added

--- boost_date_time/ptime.cc
+++ boost_date_time/ptime.cc
@@ -2,11 +2,23 @@
 #include <iostream>
 #include <boost/date_time/posix_time/posix_time.hpp>
 using namespace std;
+typedef boost::posix_time::ptime PTime;
+typedef boost::posix_time::second_clock Clock;
+
+void judgeTime(PTime time, PTime othertime){
+
+	if( time > othertime){
+		cout << "time > othertime" << endl;
+	} else if( time == othertime ){
+		cout << "time == othertime" << endl;
+	} else {
+		cout << "time < othertime" << endl;
+	}
+
+}
 
 int main()
 {
-	typedef boost::posix_time::ptime PTime;
-	typedef boost::posix_time::second_clock Clock;
 	PTime time = Clock::local_time();   // その地域を取得する
 
 	// 現在の時刻・日付・時・分・秒を取得する
@@ -17,9 +29,18 @@ int main()
 	cout << time.time_of_day().minutes() << endl;
 	cout << time.time_of_day().seconds() << endl;
 
-	// 4時間加算した時刻の日付を表示する
-	time = time + boost::posix_time::hours(4);
-	cout << time.time_of_day() << endl;
+	// 特定の時間加算・減算した時刻の日付を表示する
+	PTime time2 = time + boost::posix_time::hours(-24);
+	PTime time3 = time + boost::posix_time::hours(0);
+	PTime time4 = time + boost::posix_time::hours(24);
+	cout << time2.time_of_day() << endl;
+	cout << time3.time_of_day() << endl;
+	cout << time4.time_of_day() << endl;
+
+	// 時刻を判定することも可能です。
+	judgeTime(time, time2);
+	judgeTime(time, time3);
+	judgeTime(time, time4);
 
 	return 0;
 }
