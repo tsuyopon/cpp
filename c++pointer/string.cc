b4f95a9d626a883fe80e0f77026ea8abd6399c7f
Tsuyoshi Ooishi <tsuyoshiooishi@yahoo.co.jp>
2020-07-18 07:13:47 +0900

    add return fuction

--- c++pointer/string.cc
+++ c++pointer/string.cc
@@ -19,6 +19,36 @@ void passByPointer(std::string *teststr){
 }
 
 
+/*
+ * 
+ *  以下のドキュメントを参照のこと
+ *
+ *  best way to return an std::string that local to a function
+ *  See: https://stackoverflow.com/questions/3976935/best-way-to-return-an-stdstring-that-local-to-a-function
+ */
+
+// この場合の戻り値はローカルスコープでout of scopeにはならず、mystringのコピーが返されるようになるらしい。
+std::string returnByValue()
+{
+    std::string mystring("returnByValue");
+    return mystring;
+
+}
+
+// アドレスと参照を戻す方法の場合にはnewが必要
+// see: https://www.quora.com/How-do-you-return-std-string-from-a-function-in-C
+std::string* returnByAddress() {
+    std::string *mystring = new std::string("returnByAddress");   // アドレスや参照をreturnする場合にはnewしないとだめ!!!
+    return mystring;
+}
+
+// アドレスと参照を戻す方法の場合にはnewが必要
+std::string& returnByReference() {
+    std::string *mystring = new std::string("returnByReference");   // アドレスや参照をreturnする場合にはnewしないとだめ!!!
+    return *mystring;  // 参照なので*をつける
+}
+
+
 int main(){
 
 	// passByValueの確認
@@ -41,5 +71,17 @@ int main(){
 	std::cout << "In main: " << teststr3 << std::endl;
 	passByPointer(&teststr3);
 	std::cout << "In main: " << teststr3 << std::endl << std::endl;
+
+	// returnByValueの確認
+	std::cout << "===================== returnByValue =====================" << std::endl;
+	std::cout << "In main: " << returnByValue() << std::endl << std::endl;
+
+	// returnByAddressの確認
+	std::cout << "===================== returnByAddress =====================" << std::endl;
+	std::cout << "In main: " << *(returnByAddress()) << std::endl << std::endl;
+
+	// returnByReferenceの確認
+	std::cout << "===================== returnByReference =====================" << std::endl;
+	std::cout << "In main: " << returnByReference() << std::endl << std::endl;
 
 }
