2fea9619f2fb51c8af6d35aa07f265e581b4ae65
Tsuyoshi Ooishi <tsuyoshiooishi@yahoo.co.jp>
2021-08-09 07:02:31 +0900

    added recvmmsg.c and README.md

--- udp/sample1/README.md
+++ udp/sample1/README.md
@@ -1,3 +1,14 @@
 # 概要
 UDPで接続を行うプログラム
 ポートは22222を使っている。
+
+# コード紹介
+以下が受信側(サーバ)のサンプル
+- recvmmsg.c
+- server_recvfrom.c
+- server_recvmsg.c
+
+
+以下が送信側(クライアント)のサンプル
+- client_sendmsg.c 
+-  client_sendto.c
---
+++ udp/sample1/recvmmsg.c
@@ -0,0 +1,70 @@
+//
+// (注意点) recvmmsgシステムコールはLinux以外ではコンパイルできないことに注意してください。
+//  see: https://linuxjm.osdn.jp/html/LDP_man-pages/man2/recvmmsg.2.html
+//       https://blog.tjun.org/entry/2014/02/08/recvmmsg%E3%81%AB%E3%81%A4%E3%81%84%E3%81%A6
+//
+//  recvmsgと比較したrecvmmsgとの違い
+//   - 複数個を受け取ることができる
+//   - timeoutをとれる
+//
+#define _GNU_SOURCE
+#include <netinet/ip.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+
+#define VLEN 10
+#define BUFSIZE 200
+#define TIMEOUT 5
+
+int  main(void)  {
+
+  int sockfd, retval, i;
+  struct sockaddr_in sa;
+  struct mmsghdr msgs[VLEN];
+  struct iovec iovecs[VLEN];
+  char bufs[VLEN][BUFSIZE+1];
+  struct timespec timeout;
+
+  sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+  if (sockfd == -1) {
+    perror("socket()");
+    exit(EXIT_FAILURE);
+  }
+
+  sa.sin_family = AF_INET;
+  sa.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+  sa.sin_port = htons(22222);
+  if (bind(sockfd, (struct sockaddr *) &sa, sizeof(sa)) == -1) {
+    perror("bind()");
+    exit(EXIT_FAILURE);
+  }
+
+  memset(msgs, 0, sizeof(msgs));
+  for (i = 0; i < VLEN; i++) {
+    iovecs[i].iov_base = bufs[i];
+    iovecs[i].iov_len = BUFSIZE;
+    msgs[i].msg_hdr.msg_iov = &iovecs[i];
+    msgs[i].msg_hdr.msg_iovlen = 1;
+  }
+
+  // recvmmsgは以下でタイムアウト値を設定できるので、値を大きくしてクライアントから大量のパケットを送って、一括受信できるかテストすることが可能
+  timeout.tv_sec = TIMEOUT;
+  timeout.tv_nsec = 0;
+
+  // 一気にUDPパケットを受信してからrecvmmsgを実行すると一括で受け取ることができます。(最大はVLENで指定した10パケット まで)
+  retval = recvmmsg(sockfd, msgs, VLEN, 0, &timeout);
+  if (retval == -1) {
+    perror("recvmmsg()");
+    exit(EXIT_FAILURE);
+  }
+
+  printf("%d messages received\n", retval);
+  for (i = 0; i < retval; i++) {
+    bufs[i][msgs[i].msg_len] = 0;
+    printf("%d %s", i+1, bufs[i]);
+  }
+
+  exit(EXIT_SUCCESS);
+}
